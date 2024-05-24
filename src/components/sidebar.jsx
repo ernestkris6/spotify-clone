52fb17dc7bb8a236d396f536e0f69252ec380e8c
ERNEST CHRIS <ernestchris6@gmail.com>
2024-05-24 15:40:00 +0100

    new commit

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,12 +4,12 @@ import {assets} from '../assets/assets'
 const sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-color-white hidden lg:flex'>
-        <div className='bg-[#121212] h-[15%] rounded flex-col justify-around'>
+        <div className='bg-[#121212] pt-4 h-[15%] rounded flex-col justify-around'>
             <div className='flex items-center gap-3 pl-8 cursor-pointer'>
                 <img className='w-6' src={assets.home_icon} alt="home icon" />
                 <p className='font-bold text-white'>Home</p>
             </div>
-            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+            <div className='flex items-center gap-3 pt-4 pl-8 cursor-pointer'>
                 <img className='w-6' src={assets.search_icon} alt="home icon" />
                 <p className='font-bold text-white'>Search</p>
             </div>
