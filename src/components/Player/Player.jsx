a11bd3d14de67343c421aab7ba0b1ef3797142ea
ERNEST CHRIS <ernestchris6@gmail.com>
2024-06-06 19:32:16 +0100

    next and prev

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -4,7 +4,7 @@ import { PlayerContext } from '../Context/PlayerContext';
 
 const player = () => {
   
-  const {track, seekBar, seekBg, playStatus, play, pause, time} = useContext(PlayerContext);
+  const {track, seekBar, seekBg, playStatus, play, pause, time, previous, next} = useContext(PlayerContext);
 
 
   return (
@@ -19,12 +19,12 @@ const player = () => {
         <div className='flex flex-col items-center gap-1 m-auto'>
             <div className='flex gap-4'>
               <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
-              <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
+              <img onClick={previous} className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
               {/*Using tenary operators if playStatus is true display the pause icon else display the play icon*/}
               {playStatus ? <img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt="" /> :  <img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon} alt="" />}
              
               
-              <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
+              <img onClick={next} className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
               <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
             </div>
             <div className='flex items-center gap-5'>
