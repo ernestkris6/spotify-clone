9bd667dd3d75c6c0588280e25962497344632322
ERNEST CHRIS <ernestchris6@gmail.com>
2024-06-07 22:39:11 +0100

    cursor

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -37,7 +37,7 @@ const player = () => {
         </div>
         <div className='hidden lg:flex items-center gap-2 opacity-75'>
             <img className='w-4' src={assets.plays_icon} alt="" />
-            <img onClick={play} className='w-4' src={assets.mic_icon} alt="" />
+            <img onClick={play} className='w-4 cursor-pointer' src={assets.mic_icon} alt="" />
             <img className='w-4' src={assets.queue_icon} alt="" />
             <img className='w-4' src={assets.speaker_icon} alt="" />
             <img className='w-4' src={assets.volume_icon} alt="" />
