84bf59b3c93ec4098511a816b48cac4754e480b2
ERNEST CHRIS <ernestchris6@gmail.com>
2024-08-01 20:51:26 +0100

    music

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,8 +21,8 @@ const Navbar = () => {
                 </div>
             </div>
             <div className='flex items-center gap-2 mt-4'>
-                <p className='bg-white text-black px-4 rounded-2xl cursor-pointer'>All</p>
-                <p className='bg-black py-1 px-4 rounded-2xl cursor-pointer'>Music</p>
+                <p className='bg-white text-black px-4 rounded-2xl cursor-pointer' onClick={()=> navigate('/')}>All</p>
+                <p className='bg-black py-1 px-4 rounded-2xl cursor-pointer' onClick={()=> navigate("/album/:id")}>Music</p>
                 <p className='bg-black py-1 px-4 rounded-2xl cursor-pointer'>Podcasts</p>
             </div>
         </>
